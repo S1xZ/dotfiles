892ed27fb9e34176178f5961fb47811a35cdeb7c
Punya Gunawardana <punya.gnwdn@gmail.com>
2023-11-25 00:27:49 +0700

    config of: Sat Nov 25 12:27:49 AM +07 2023

--- sync.sh
+++ sync.sh
@@ -2,7 +2,12 @@
 
 current_date_time=$(date)
 
-cp ~/.config .
+# Nvim
+cp -r ~/.config/nvim ./nvim
+
+# Kitty
+cp -r ~/.config/kitty ./kitty
+
 git add .
 git commit -m "config of: $current_date_time"
 git push --force
