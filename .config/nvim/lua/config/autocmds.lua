0b739eb21b485c789fe02d72d890ffeac5839fa5
S1xZer <S1xZ@noreply.com>
2024-08-26 14:23:44 +0700

    Updates configs

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
+  group = vim.api.nvim_create_augroup("Color", {}),
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "LspReferenceRead", { fg = "#FF0000" })
+    vim.api.nvim_set_hl(0, "LspReferenceWrite", { fg = "#FF0000" })
+    vim.api.nvim_set_hl(0, "LspReferenceText", { fg = "#FF0000" })
+  end,
+})
