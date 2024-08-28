18c70911963f9f0b6c6b2309f76c2e05e5592e9e
S1xZer <S1xZ@noreply.com>
2024-08-29 05:14:13 +0700

    Update config/lockfile

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,8 +1,10 @@
 return {
   "stevearc/oil.nvim",
   cmd = "Oil",
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  opts = {},
+  --dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  opts = { default_file_explorer = true, view_options = {
+    show_hidden = true,
+  } },
   keys = {
     { "-", mode = { "n" }, "<CMD>Oil<CR>", { desc = "Open parent directory" } },
   },
