2169a7ed3dba056d80ae550456a5a6260b1a91ef
Pny G. <77716688+S1xZ@users.noreply.github.com>
2024-08-30 10:38:23 +0700

    Add wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,42 @@
+local wezterm = require("wezterm")
+local launch_menu = {}
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Define launch_menu
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_prog = { "ubuntu2204.exe" }
+	table.insert(launch_menu, {
+		label = "PowerShell",
+		args = { "pwsh.exe", "-NoLogo" },
+	})
+
+	-- Add WSL Ubuntu entry
+	table.insert(launch_menu, {
+		label = "WSL Ubuntu",
+		args = { "ubuntu2204.exe" },
+		cwd = "~",
+	})
+end
+
+config.keys = {}
+
+config.launch_menu = launch_menu
+
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font_with_fallback({
+	"JetBrainsMono Nerd Font",
+	"Noto Sans Mono CJK JP",
+	"SOV_monospace",
+})
+config.font_size = 15
+
+-- Disable Windows decoration
+config.window_decorations = "RESIZE"
+
+-- Tabs config
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+
+return config
