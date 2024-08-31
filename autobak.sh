44c4eed0cb500ad34c32c89ddcebdf25079061bc
S1xZer <s1xzer@no-reply.com>
2024-08-31 15:15:54 +0700

    Fix autobak

--- autobak.sh
+++ autobak.sh
@@ -7,7 +7,13 @@ cp ${HOME}/.zshrc .
 cp -r ${HOME}/.config/nvim ./.config/nvim
 
 # Ohmyposh
-cp -r ${HOME}/.config/ohmyposh ./.config/ohmyposh/
+cp -r ${HOME}/.config/ohmyposh ./.config/ohmyposh
 
 # tmux
 cp -r ${HOME}/.config/tmux ./.config/tmux
+
+# qtile
+cp -r ${HOME}/.config/qtile ./.config/qtile
+
+# picom
+cp -r ${HOME}/.config/picom ./.config/picom
