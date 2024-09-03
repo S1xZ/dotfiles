f0ee17cbe6b7627251e84cc814fce7a10cba4161
S1xZer <s1xzer@no-reply.com>
2024-09-03 20:30:56 +0700

    Update nvim & remove missed rasi symlink

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,18 @@
 return {
+
   { "catppuccin/nvim", lazy = true, opts = { flavour = "mocha" } },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      document_highlight = {
+        enabled = false,
+      },
     },
   },
 }
