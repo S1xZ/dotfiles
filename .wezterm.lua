2969ce04cfbb9ab8e4799a3b7569d463a38ecdca
s1xzer <s1xzer@no-reply.com>
2024-09-05 14:26:47 +0700

    update config

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,34 +6,34 @@ local config = wezterm.config_builder()
 
 -- Define launch_menu
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "ubuntu2204.exe" }
-	table.insert(launch_menu, {
-		label = "PowerShell",
-		args = { "pwsh.exe", "-NoLogo" },
-	})
+  config.default_prog = { "ubuntu2204.exe" }
+  table.insert(launch_menu, {
+    label = "PowerShell",
+    args = { "pwsh.exe", "-NoLogo" },
+  })
 
-	-- Add WSL Ubuntu entry
-	table.insert(launch_menu, {
-		label = "WSL Ubuntu",
-		args = { "ubuntu2204.exe" },
-		cwd = "~",
-	})
+  -- Add WSL Ubuntu entry
+  table.insert(launch_menu, {
+    label = "WSL Ubuntu",
+    args = { "ubuntu2204.exe" },
+    cwd = "~",
+  })
 end
 
 config.keys = {}
 
 config.launch_menu = launch_menu
 
+-- Themes
 config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font_with_fallback({
-	"JetBrainsMono Nerd Font",
-	"Noto Sans Mono CJK JP",
-	"SOV_monospace",
-})
-config.font_size = 15
 
--- Disable Windows decoration
-config.window_decorations = "RESIZE"
+config.font = wezterm.font_with_fallback({
+  "JetBrainsMono Nerd Font",
+  "Noto Sans Mono CJK JP",
+  "SOV_monospace",
+})
+
+config.font_size = 15
 
 -- Tabs config
 config.hide_tab_bar_if_only_one_tab = true
