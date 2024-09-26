f3359aec33abbb51878bab60889b8cf495073d69
s1xzer <s1xzer@no-reply.com>
2024-09-26 20:02:41 +0700

    Update Nvim config and lazylock

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,15 +1,22 @@
 return {
-  "stevearc/oil.nvim",
-  cmd = "Oil",
-  lazy = false,
-  opts = {
-    default_file_explorer = true,
-    view_options = {
-      show_hidden = true,
+  {
+    "stevearc/oil.nvim",
+    cmd = "Oil",
+    lazy = false,
+    opts = {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
     },
+    keys = {
+      { "-", mode = { "n" }, "<CMD>Oil<CR>", { desc = "Open parent directory" } },
+    },
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
   },
-  keys = {
-    { "-", mode = { "n" }, "<CMD>Oil<CR>", { desc = "Open parent directory" } },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {},
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
   },
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
 }
