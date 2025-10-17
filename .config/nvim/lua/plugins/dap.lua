77603bda6a312a1af8468733a5ac258b8fd073f4
Punya Gunawardana <punya.gunawardana@agoda.com>
2025-10-17 22:17:06 +0700

    update

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -23,8 +23,6 @@ return {
     desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
 
     dependencies = {
-      "rcarriga/nvim-dap-ui",
-      -- virtual text for the debugger
       {
         "theHamsta/nvim-dap-virtual-text",
         opts = {},
